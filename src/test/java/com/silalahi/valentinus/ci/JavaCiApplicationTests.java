32a26e80d8abf58833965eb087a075f49992f72f
valentinus <valentinus.silalahi@gmail.com>
2016-10-15 02:28:59 +0700

    the first template project

---
+++ src/test/java/com/silalahi/valentinus/ci/JavaCiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.silalahi.valentinus.ci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaCiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
